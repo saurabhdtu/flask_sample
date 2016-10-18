b7766e6046a5875ae7fbaf38758f36a766a5d124
saurabh <saurabhlahoti@kalagato.com>
2016-10-18 14:02:38 +0530

    saved

--- server/__init__.py
+++ server/__init__.py
@@ -4,43 +4,16 @@ import time
 from flask import url_for, redirect, request, render_template, make_response, abort, jsonify
 
 from server.views import user
+from server.utils import file_utils
 from server.views.validators import validator
 from .database import app, cursor
 
 app.register_blueprint(user.mod)
 
-@app.route('/')
-def hello_world():
-    return render_template('Hello.html')
-
-
-@app.route("/hello/<path>")
-def myfunc(path):
-    return path + "value"
-
-
-@app.route("/hello/<path>/<userId>")
-def myCustomFunc(path, userId):
-    return path + "   " + userId + "  value"
-
-
-@app.route("/admin")
-def login():
-    return "you are admin"
-
-
-@app.route("/user/<username>")
-def user(username):
-    return username
-
-
-@app.route("/access/<type>")
-def handleLogin(type):
-    if type.lower() == "admin":
-        return redirect(url_for('login'))
-    else:
-        return redirect(url_for('user', username=type))
-
+@app.route('/', defaults={'name': 'Saurabh'})
+@app.route('/<name>')
+def hello_world(name):
+    return render_template('Hello.html', name=name)
 
 @app.route("/file", methods=['POST'])
 def register():
@@ -50,7 +23,7 @@ def register():
         if validator.validateFile(uploadedFile.filename):
             APP_ROOT = os.path.dirname(os.path.abspath(__file__))
             UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads/')
-            uploadedFile.save(UPLOAD_FOLDER + str(int(time.time())) + "." + server.utils.file_utils.getFileExt(uploadedFile.filename))
+            uploadedFile.save(UPLOAD_FOLDER + str(int(time.time())) + "." + file_utils.getFileExt(uploadedFile.filename))
             make_response()
             return "success"
         else:
@@ -88,24 +61,6 @@ def killed():
 @app.route('/redirect')
 def redir():
     return redirect(url_for('killed'))
-
-
-@app.route('/users')
-def users():
-    return getUsers()
-
-
-def getUsers():
-    cursor.execute("select name,email from user")
-    data = cursor.fetchall()
-    response = {}
-    list = []
-    for s in data:
-        aDict = {}
-        aDict['name'] = s[0]
-        aDict['email'] = s[1]
-        list.append(aDict)
-    return jsonify(data=list)
 
 
 def error_response(error):
